bd4d78f15f946f25465833866bcad2cc2eca0678
Piyush Dharmesh Mistry <piyushmistry12300@gmail.com>
2026-09-09 20:54:44 +0530

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+
+        for(String str:strs){
+            sb.append(str.length());
+            sb.append('#');
+            sb.append(str);
+        }
+
+        return sb.toString();
+
+    }
+
+    public List<String> decode(String str) {
+        int i=0;
+        List<String> result = new ArrayList<>();
+
+        while(i<str.length()){
+            int j=i;
+            while(str.charAt(j)!='#'){
+                j++;
+            }
+            int len = Integer.parseInt(str.substring(i,j));
+
+            String word = str.substring(j+1,j+len+1);
+            result.add(word);
+
+            i=j+len+1;
+        }
+        return result;
+    }
+}
